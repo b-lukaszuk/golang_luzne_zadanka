5cb38feaf1dfab420f08d145dc4dbed70b331759
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-12 18:12:53 +0200

    added book1/ch09/ booleanExpressions/ and comparisonOperators/

---
+++ book1/ch09/comparisonOperators/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 21
+	b := 42
+
+	fmt.Println(a == b)
+	fmt.Println(a == a)
+	fmt.Println(a != b)
+	fmt.Println(a > b)
+	fmt.Println(a < b)
+	fmt.Println(a >= b)
+	fmt.Println(a <= b)
+	fmt.Println(a >= a)
+	fmt.Println(a <= a)
+}
