38c2211ee529c34e95d9e25ca5eef8671045c1f9
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-16 10:20:20 +0200

    added book2/ch03/example1/

---
+++ book2/ch03/example1/go.mod
@@ -0,0 +1,4 @@
+// generated with bash command: go mod init example1
+module example1
+
+go 1.18
---
+++ book2/ch03/example1/integers/adder_test.go
@@ -0,0 +1,23 @@
+// running tests with verbose mode: go test -v
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	// the comment below required for this test to run
+	fmt.Println(sum)
+	// Output: 6
+}
