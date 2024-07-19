bc50862d46330300fb177c5799904744996ca138
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-19 16:17:22 +0200

    added book2/ch07/example4/

---
+++ book2/ch07/example4/wallet/wallet_test.go
@@ -0,0 +1,17 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+	want := Bitcoin(11)
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
