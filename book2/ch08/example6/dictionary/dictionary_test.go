b57967dcd9a5d1f415f74535becc6994fae1519e
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-21 08:24:34 +0200

    added book2/ch08/example6/

---
+++ book2/ch08/example6/dictionary/dictionary_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"testing"
+)
+
+func assertStrings(t testing.TB, got string, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q, want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary,
+	word string, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
+}
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
