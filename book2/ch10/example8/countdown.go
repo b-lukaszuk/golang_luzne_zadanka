05d087cbd1d140cd742333e2eadbda645e834375
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-23 19:05:45 +0200

    added book2/ch10/example8/

---
+++ book2/ch10/example8/countdown.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+type SpyCountdownOperations struct {
+	Calls []string // each time it is called info is put to Calls
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration       // duration of sleep
+	sleep    func(time.Duration) // sleep function itself
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprintln(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
