194ff4512a9398a7ad9f5cfa495965a625c4f081
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-09-03 21:18:50 +0200

    added solution to book4/ch1/t1

---
+++ book4/ch1/t1/helloName.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Toy program.\n")
+	fmt.Printf("It prints my name.\n\n")
+
+	fmt.Printf("Hello, my name is Bart.\n\n")
+
+	fmt.Println("That's all. Goodbye!")
+}
