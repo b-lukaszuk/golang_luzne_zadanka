34365d71f3c01b37e69c0a5cc91c98abea12b9e0
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-10-20 08:32:20 +0200

    added sudoku solver

--- book3/ch29/t1/sudokuSolver.go
+++ book3/ch29/t1/sudokuSolver.go
@@ -14,14 +14,14 @@ const (
 	width       = 9
 	height      = 9
 	empty       = 0
-	path2Sudoku = "./sudoku.txt"
+	path2Sudoku = "./sudoku_hard.txt"
 )
 
 func getWords(path string) ([]string, error) {
 	// content - byteslice, defer f.Close() is inside ReadFile
 	content, err := os.ReadFile(path)
 	if err != nil {
-		return make([]string, 0), err
+		return make([]string, empty), err
 	}
 	text := string(content)         // byte slice to string
 	textArr := strings.Fields(text) // split on whitespaces with Fields
@@ -112,6 +112,57 @@ func isCollisionIn3x3(num uint64, curRow, curCol uint,
 	return false
 }
 
+// is there a collision after placing a num in the field
+func isCollision(num uint64, curRow, curCol uint,
+	sudoku *[height][width]uint64) bool {
+	return isCollisionIn3x3(num, curRow, curCol, sudoku) ||
+		isCollisionInRow(num, curRow, curCol, sudoku) ||
+		isCollisionInCol(num, curRow, curCol, sudoku)
+}
+
+func isSolved(sudoku *[height][width]uint64) bool {
+	var sum uint64 = 0
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			if sudoku[r][c] == empty {
+				return false
+			}
+			sum += sudoku[r][c]
+		}
+	}
+	return (sum == 405) // cause sum(1:9)*9, collision tests prevent duplicates
+}
+
+// modifies sudoku inplace, prints solution
+func solve(sudoku *[height][width]uint64) {
+	for r := uint(0); r < height; r++ {
+		for c := uint(0); c < width; c++ {
+			if sudoku[r][c] != empty {
+				continue
+			}
+			for i := uint64(1); i < 10; i++ {
+				if isCollision(i, r, c, sudoku) {
+					continue
+				}
+				// tryout a value into a cell
+				sudoku[r][c] = i
+				if isSolved(sudoku) {
+					// for solved sudoku
+					printSudoku(sudoku)
+					fmt.Println()
+					return
+				}
+				// try to solve with that i value (see above) in
+				solve(sudoku)
+				// if that failed, assign empty again and try different i
+				sudoku[r][c] = empty
+			}
+			return // if i = 1:9, does not solve then terminate
+		}
+	}
+	return // terminate after passing all fields, when no solution found
+}
+
 func main() {
 	fmt.Printf("Toy program.\n")
 	fmt.Printf("It solves a sudoku puzzle (if its solvable).\n\n")
@@ -120,6 +171,8 @@ func main() {
 	if err == nil {
 		printSudoku(&sudoku)
 		fmt.Printf("\n")
+		fmt.Printf("Solution(s) (empty line if none found):\n\n")
+		solve(&sudoku)
 	}
 
 	fmt.Printf("\nThat's all. Goodbye!\n")
