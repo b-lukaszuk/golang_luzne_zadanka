3ed73a1b3112aa3a9a86461397a13c66b20dace0
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-11-02 22:33:52 +0100

    added book4/ch9/t1 with initial contents

---
+++ book4/ch9/t1/math.go
@@ -0,0 +1,11 @@
+// simple solution,
+// not idiot/error-proof, not optimized for speed
+package math
+
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
+}
